049c19a3f58d646b8e116c947866f8ff7da1195d
dealeader <huglesmafia@gmail.com>
2021-11-22 21:10:20 +0500

    added an instances of point, vector; added some tests for class methods

--- index.php
+++ index.php
@@ -20,3 +20,31 @@ $magic->__invoke();
 $magic->__set_state(0);
 $magic->__clone();
 $magic->__debugInfo();
+
+use App\Point;
+use App\Vector;
+
+echo "<h1> Point tests </h1>" . "<br>";
+
+$T1 = new Point();
+echo $T1;
+
+$T1 = $T1->axisTransfer(1, 1);
+echo $T1;
+
+$V1 = new Vector(4, 3);
+$V2 = new Vector(0, 0);
+$V3 = new Vector(-3, 4);
+
+echo "<h1> Vectors Lengths </h1>" . "<br>";
+$V1->vectorLength();
+$V2->vectorLength();
+$V3->vectorLength();
+$V2->isNullVector();
+
+echo "<h1> Perpendicular </h1>" . "<br>";
+Vector::isPerpendicularVector($V1, $V3);
+
+$T1 = $T1->axisTransfer(4, 3);
+
+
