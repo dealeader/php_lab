4b1abac1f83bc75a1ae597ae4451f35c92ecff21
dealeader <huglesmafia@gmail.com>
2021-11-26 15:29:14 +0500

    fixed return values

--- src/Vector.php
+++ src/Vector.php
@@ -12,18 +12,18 @@ class Vector {
         $this->y = $y;
     }
 
-    public function vectorLength()
+    public function vectorLength(): float
     {
-        printf("%.2f" . "<br>", sqrt($this->x ** 2 + $this->y ** 2)); 
+        return sqrt($this->x ** 2 + $this->y ** 2); 
     }
 
-    public function isNullVector()
+    public function isNullVector(): bool
     {
-        echo (($this->x == 0) and ($this->y == 0)) ? "Vector is nullable" . "<br>" : "Vector is not nullable" . "<br>";
+        return ($this->x == 0) && ($this->y == 0);
     }
 
-    public static function isPerpendicularVector(Vector $v1, Vector $v2) 
+    public function isPerpendicularVector( Vector $v) 
     {
-        echo ($v1->x * $v2->x + $v1->y * $v2->y == 0) ? "Vectors are perpendicular" . "<br>" : "Vectors are not perpendicular" . "<br>";
+        return ($this->x * $v->x + $this->y * $v->y == 0);
     }
 }
